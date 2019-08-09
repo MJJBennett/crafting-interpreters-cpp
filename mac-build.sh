a653e3f26afde8394aeb2f9bab6f0fd4ff6cf211
Michael Bennett <michaelbennett.dev@gmail.com>
2019-08-08 23:37:44 -0700

    Make build script better.

--- mac-build.sh
+++ mac-build.sh
@@ -1,5 +1,7 @@
+cd output >/dev/null 2>&1
 cmake .. -DCMAKE_CXX_COMPILER=/usr/local/bin/g++ -DCMAKE_C_COMPILER=/usr/local/bin/gcc-8
 # rm ../compile_commands.json
 # ln -s compile_commands.json ../compile_commands.json
 cp compile_commands.json ..
-./fix-compile-commands.py ../include ../src
+cd ..
+./fix-compile-commands.py include src
