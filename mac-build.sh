b186c91efb1321fdfa9583866d78be53d3ee4c16
Michael Bennett <michaelbennett.dev@gmail.com>
2019-08-08 20:52:58 -0700

    Fix compile_commands to work properly with YCM.

---
+++ mac-build.sh
@@ -0,0 +1,4 @@
+cmake .. -DCMAKE_CXX_COMPILER=/usr/local/bin/g++ -DCMAKE_C_COMPILER=/usr/local/bin/gcc-8
+# rm ../compile_commands.json
+# ln -s compile_commands.json ../compile_commands.json
+cp compile_commands.json ..
