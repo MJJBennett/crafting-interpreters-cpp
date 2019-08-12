7b55b845a3acca5ae1c6d0cc21488a93899f37c2
Michael Bennett <michaelbennett.dev@gmail.com>
2019-08-11 21:04:28 -0700

    Fix non-mac build script, maybe.

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+cd output >/dev/null 2>&1
 cmake .. -DCMAKE_CXX_COMPILER=/usr/bin/g++-9 -DCMAKE_C_COMPILER=/usr/bin/gcc-9 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 # ln -s compile_commands.json ../compile_commands.json
 cp compile_commands.json ..
+cd ..
+./fix-compile-commands.py include src
